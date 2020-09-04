62377d559093ca48c2f4e77133c3a6abe3b90fd6
Melisa Budini <melixbq@gmail.com>
2020-09-04 12:25:42 -0400

    act t.factura, +t.producto, act modelo, +tabla listado

--- prueba.sql
+++ prueba.sql
@@ -9,3 +9,19 @@ CREATE TABLE factura(numero_factura SERIAL PRIMARY KEY,
 cliente_id INT NOT NULL, fecha_factura DATE, subtotal
 INT NOT NULL, iva FLOAT NOT NULL, precio_total INT NOT NULL);
 
+CREATE TABLE producto(id SERIAL PRIMARY KEY,
+nombre VARCHAR(50), descripcion VARCHAR(100),
+valor_unitario INT NOT NULL, categoria_id
+INT NOT NULL, FOREIGN KEY(categoria_id)
+REFERENCES categoria(id));
+
+
+CREATE TABLE listado_productos(
+producto_id INT NOT NULL, cantidad INT NOT NULL,
+valor_total_producto INT, factura_id
+INT NOT NULL, FOREIGN KEY(producto_id)
+REFERENCES producto(id), FOREIGN KEY
+(factura_id) REFERENCES factura(numero_factura));
+
+
+
